682a1856017d16cfca035b3e732e48223cca5774
Steven <stanli-peter19@hotmail.com>
2021-03-24 09:00:02 +0000

    simple counter with react

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -3,17 +3,17 @@ import PropType from "prop-types";
 
 export const Counter = props => {
 	return (
-		<div className="container bg-dark text-white ">
-			<div className="row d-flex justify-content-center">
+		<div className="container bg-secondary bg-gradient text-white rounded-pill">
+			<div className="row d-flex justify-content-center box">
 				<span className="col">
-					<i className="far fa-clock" />
+					<i className="far fa-clock clock" />
 				</span>
-				<span className="col">{props.secSix % 10}</span>
-				<span className="col">{props.secFive % 10}</span>
-				<span className="col">{props.secFour % 10}</span>
-				<span className="col">{props.secThree % 10}</span>
-				<span className="col">{props.secTwo % 10}</span>
-				<span className="col">{props.secOne % 10}</span>
+				<span className="col count">{props.secSix % 10}</span>
+				<span className="col count">{props.secFive % 10}</span>
+				<span className="col count">{props.secFour % 10}</span>
+				<span className="col count">{props.secThree % 10}</span>
+				<span className="col count">{props.secTwo % 10}</span>
+				<span className="col count">{props.secOne % 10}</span>
 			</div>
 		</div>
 	);
